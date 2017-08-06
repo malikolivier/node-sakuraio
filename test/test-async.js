86f5fc4bf405bf022c24d1cd512b1934829c82b1
Malik Olivier Boussejra <malik@boussejra.com>
2017-08-06 12:30:44 +0900

    Rewrite test file using mocha

--- test/test-async.js
+++ test/test-async.js
@@ -1,16 +1,41 @@
+/* eslint-env mocha */
+const assert = require('assert')
 const SakuraIOSim = require('./sakuraio-sim')
 
-SakuraIOSim.open(function (err, bus) {
-  if (err) throw err
-  bus.getConnectionStatus(function (err, status) {
-    if (err) throw err
-    console.log(`ASYNC1: ${status}`)
-  })
-  bus.getConnectionStatus(function (err, status) {
-    if (err) throw err
-    console.log(`ASYNC2: ${status}`)
+describe('Async and sync', function () {
+  it('should return the same result', function (done) {
+    var bus = SakuraIOSim.openSync()
+    var syncResult = bus.getConnectionStatusSync()
+    SakuraIOSim.open(function (err, bus) {
+      if (err) throw err
+      bus.getConnectionStatus(function (err, asyncResult) {
+        if (err) throw err
+        assert.equal(syncResult, asyncResult)
+        done()
+      })
+    })
   })
 })
 
-var bus = SakuraIOSim.openSync()
-console.log(`SYNC: ${bus.getConnectionStatusSync()}`)
+describe('Async', function () {
+  it('should wait for first command to complete before starting second', function (done) {
+    SakuraIOSim.open(function (err, bus) {
+      if (err) throw err
+      var status1, status2
+      bus.getConnectionStatus(function (err, status) {
+        if (err) throw err
+        status1 = status
+        if (status2 !== undefined) {
+          throw new Error('Second command finished first!')
+        }
+      })
+      bus.getConnectionStatus(function (err, status) {
+        if (err) throw err
+        status2 = status
+        if (status1 !== undefined) {
+          done()
+        }
+      })
+    })
+  })
+})
